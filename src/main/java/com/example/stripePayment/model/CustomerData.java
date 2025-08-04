f8bbfaf205feb02e4cc50b40d6d9dc2d7e85ab4e
Ignacio Molina <molinagranda@gmail.com>
2025-08-04 15:30:19 -0300

    CustomerModel Creado

---
+++ src/main/java/com/example/stripePayment/model/CustomerData.java
@@ -0,0 +1,12 @@
+package com.example.stripePayment.model;
+
+import lombok.Data;
+
+@Data
+public class CustomerData {
+
+    private String email;
+    private String name;
+    private String customerId;
+
+}
