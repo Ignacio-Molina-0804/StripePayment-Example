787cf20322fb634c1086356c10ed413c8f9997e2
Ignacio Molina <molinagranda@gmail.com>
2025-08-05 14:25:20 -0300

    AllCustomer Funcional en index

--- src/main/java/com/example/stripePayment/controller/StripePaymentController.java
+++ src/main/java/com/example/stripePayment/controller/StripePaymentController.java
@@ -1,17 +1,20 @@
 package com.example.stripePayment.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.stripePayment.model.CustomerData;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
-
+import com.stripe.model.CustomerCollection;
+import com.example.stripePayment.model.CustomerData;
 
 @Controller
 public class StripePaymentController {
@@ -19,9 +22,26 @@ public class StripePaymentController {
     @Value("${stripe.apikey}")
     String stripeApiKey;
 
-
     @RequestMapping("/index")
-    public String index() {
+    public String index(Model model) throws StripeException {
+
+        Stripe.apiKey = stripeApiKey;
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("limit", 3);
+
+        CustomerCollection customers = Customer.list(params);
+        List<CustomerData> allCustomer = new ArrayList<CustomerData>();
+        for (int i = 0; i < customers.getData().size(); i++) {
+            CustomerData customerData = new CustomerData();
+            customerData.setCustomerId(customers.getData().get(i).getId());
+            customerData.setName(customers.getData().get(i).getName());
+            customerData.setEmail(customers.getData().get(i).getEmail());
+            allCustomer.add(customerData);
+        }
+
+
+        model.addAttribute("customers", allCustomer);
         return "index";
     }
 
@@ -30,18 +50,16 @@ public class StripePaymentController {
         return "create-customer";
     }
 
-    @RequestMapping("/addCustomer")
-    public String addCustomer(CustomerData customerData) throws StripeException{
-        Stripe.apiKey = stripeApiKey;
-        Map<String, Object> params = new HashMap<>();
-        params.put("email", customerData.getEmail());
-        params.put("name", customerData.getName());
-        Customer customer = Customer.create(params);
-
-        return "success";
-    }
-
-    
-    
+	@RequestMapping("/addCustomer")
+	public String addCustomer(CustomerData customerData) throws StripeException {
+		
+		Stripe.apiKey = stripeApiKey;
+		Map<String, Object> params = new HashMap<>();
+		params.put("name", customerData.getName());
+		params.put("email", customerData.getEmail());
+		Customer customer = Customer.create(params);
+		
+		return "success";
+	}
 
 }
